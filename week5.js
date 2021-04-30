b347160794345809b02cc824e105fc4faae8ab0d
Patrick Dorion <patrick.dorion@kellogg.nothwestern.edu>
2021-04-30 12:47:45 -0500

    v3

--- week5.js
+++ week5.js
@@ -7,7 +7,7 @@
 //   - days = number of days of forecast data to return, between 1-10
 // - Example: https://api.weatherapi.com/v1/forecast.json?key=YOUR-API-KEY&q=Chicago&days=3
 
-//https://api.weatherapi.com/v1/forecast.json?key=85c7eec3fadd4fe0ade12006213004&q=Chicago&days=3
+//My API URL with my key is   https://api.weatherapi.com/v1/forecast.json?key=85c7eec3fadd4fe0ade12006213004&q=Chicago&days=3
 
 // - The basic recipe (algorithm) is included; write the rest of the recipe in the comments!
 // - Lab: Follow the provided recipe and the "mock-up" provided in the hard-coded HTML; respond 
@@ -17,7 +17,11 @@
 // - Homework: Complete the application by accepting a number of days; show the current weather 
 //             conditions and forecast based on the number of days entered by the user.
 
+
+//Trigger load content on loading of the DOM
+
 window.addEventListener('DOMContentLoaded', async function() {
+    
     // Get a reference to the "get weather" button
     let getWeatherButton = document.querySelector(`.get-weather`)
     // When the "get weather" button is clicked:
@@ -26,25 +30,32 @@ window.addEventListener('DOMContentLoaded', async function() {
       event.preventDefault()
       // - Get a reference to the element containing the user-entered location
       let inputLocationElement = document.querySelector(`#location`)
-      // - Get the user-entered location from the element's value
+      // - Get the user-entered location from the element's value, check in console
       let inputLocation = inputLocationElement.value
       console.log(inputLocation)
   
   
-      //get number of days to put into fetch
+    //Repeat the steps above to get the value of days entered (1-3 day range)
+    // When the "get weather" button is clicked:
+    
+    // - Get a reference to the element containing the user-entered forecast length
+    let inputDaysElement = document.querySelector(`#days`)
+    // - Get the user-entered location from the element's value, check in console
+    let inputDays = inputDaysElement.value
+    console.log(inputDays)
   
-      // - Check to see if the user entered anything; if so:
-      if (inputLocation.length > 0) {
+      // - Check to see if the user entered anything in both fields; if so:
+      if (inputLocation.length > 0 && inputDays.length > 0) {
   
         // - Construct a URL to call the WeatherAPI.com API
-        let url = `https://api.weatherapi.com/v1/forecast.json?key=85c7eec3fadd4fe0ade12006213004&q=${inputLocation}&days=3`
+        let url = `https://api.weatherapi.com/v1/forecast.json?key=85c7eec3fadd4fe0ade12006213004&q=${inputLocation}&days=${inputDays}`
         // - Fetch the url, wait for a response, store the response in memory
         let response = await fetch(url)
   
         // - Ask for the json-formatted data from the response, wait for the data, store it in memory
         let json = await response.json(url)
         
-        // - Write the json-formatted data to the JavaScript console
+        // - Write the json-formatted data to the JavaScript console as a check
         console.log(json)
       
         // - Store the interpreted location, current weather conditions, the forecast as three separate variables
@@ -53,15 +64,15 @@ window.addEventListener('DOMContentLoaded', async function() {
         let location = `${city}, ${region}`
         console.log(location)
   
-  
         let temp = json.current.temp_f
         let condition = json.current.condition.text
         let currentweather = `${temp} and ${condition}`
         console.log(currentweather)
   
-        
+        //get a reference to the current weather div to update with the live weathe info 
         let currentElement = document.querySelector(`.current`)
   
+        //inject the retrieved current weather and location info into the html document to display the current info
         currentElement.innerHTML = `
         <div class="text-center space-y-2">
         <div class="font-bold text-3xl">Current Weather for ${location}</div>
@@ -73,17 +84,15 @@ window.addEventListener('DOMContentLoaded', async function() {
         </div>
       </div>
         `
-  
+        
   
   
   
         
-        
-        
-        // - Continue the recipe yourself!
+    
         //make a loop for pulling the forecast from the weather API
         // for (i = 0, i < 3, i++) {
-        //   let datee = json.forecast.forecastday[i].date
+        //   let date = json.forecast.forecastday[i].date
         //   console.log(date)
         // }
       }
