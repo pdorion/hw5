3cc5c249506f853dd14dfaa0cf04adf38ad488e3
Patrick Dorion <patrick.dorion@kellogg.nothwestern.edu>
2021-04-30 16:19:51 -0500

    v4

--- week5.js
+++ week5.js
@@ -18,6 +18,10 @@
 //             conditions and forecast based on the number of days entered by the user.
 
 
+
+
+
+
 //Trigger load content on loading of the DOM
 
 window.addEventListener('DOMContentLoaded', async function() {
@@ -40,7 +44,7 @@ window.addEventListener('DOMContentLoaded', async function() {
     
     // - Get a reference to the element containing the user-entered forecast length
     let inputDaysElement = document.querySelector(`#days`)
-    // - Get the user-entered location from the element's value, check in console
+    // - Get the user-entered forecast length from the form element's value, check in console
     let inputDays = inputDaysElement.value
     console.log(inputDays)
   
@@ -58,18 +62,15 @@ window.addEventListener('DOMContentLoaded', async function() {
         // - Write the json-formatted data to the JavaScript console as a check
         console.log(json)
       
-        // - Store the interpreted location, current weather conditions, the forecast as three separate variables
+        // - Store the interpreted location, current weather conditions, and the forecast as separate variables
         let region = json.location.region 
         let city = json.location.name
         let location = `${city}, ${region}`
-        console.log(location)
   
         let temp = json.current.temp_f
         let condition = json.current.condition.text
-        let currentweather = `${temp} and ${condition}`
-        console.log(currentweather)
   
-        //get a reference to the current weather div to update with the live weathe info 
+        //get a reference to the current weather div to update with the live weather info 
         let currentElement = document.querySelector(`.current`)
   
         //inject the retrieved current weather and location info into the html document to display the current info
@@ -82,19 +83,53 @@ window.addEventListener('DOMContentLoaded', async function() {
           and
           <span class="conditions">${condition}</span>
         </div>
-      </div>
+        </div>
         `
-        
+
+        //get a reference to the forcast weather div to update with the forecast info
+        let forecastElement = document.querySelector(`.forecast`)
+
+        //update the forecast display length
+        forecastElement.innerHTML = `
+
+          <div class="forecast space-y-4">
+          
+          <div class="text-center space-y-8">
+            <div class="font-bold text-3xl">${inputDays} Day Forecast</div>
+            
+
+
+            </div>
+        `
+
+        //make a loop for pulling the forecast from the weather API and add the info to the html dom until all forecasts requested are visible
+        for (i = 0; i < json.forecast.forecastday.length; i++) {
+            //define variables to use in the loop for each forcasted day
+          let date = json.forecast.forecastday[i].date
+          let conditionForecasted = json.forecast.forecastday[i].day.condition.text
+          let low = json.forecast.forecastday[i].day.mintemp_f
+          let hi = json.forecast.forecastday[i].day.maxtemp_f
+          let pic = json.forecast.forecastday[i].day.condition.icon
+
+          //insert this string as html for each iteration until all days in the forecast are done
+          forecastElement.insertAdjacentHTML('beforeend', `
+
+          <div class="text-center space-y-8 font-bold text-2xl">
+
+          <div>
+          <img src="https:${pic}" class="mx-auto">
+          <h1 class="text-2xl text-bold text-gray-500">${date}</h1>
+          <h2 class="text-xl">High ${hi}° – Low ${low}°</h2>
+          <p class="text-gray-500">${conditionForecasted}</h1>
+        </div>
+
+        </div>
+        `)
+        }   
+
   
-  
-  
-        
     
-        //make a loop for pulling the forecast from the weather API
-        // for (i = 0, i < 3, i++) {
-        //   let date = json.forecast.forecastday[i].date
-        //   console.log(date)
-        // }
       }
+ 
       })
   })
